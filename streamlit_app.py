3dc79411e3422a7c4942f042fd12faf31453c152
PerNots <75035875+PerNots@users.noreply.github.com>
2025-01-08 12:56:42 +0000

    trying to show columns next to each other on mobile

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+st.write('''<style>
+
+[data-testid="column"] {
+    width: calc(33.3333% - 1rem) !important;
+    flex: 1 1 calc(33.3333% - 1rem) !important;
+    min-width: calc(33% - 1rem) !important;
+}
+</style>''', unsafe_allow_html=True)
+
 # Drink prices
 prices = {
     "Wasser": 3.50,
